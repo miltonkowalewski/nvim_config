eba15da1aa86d5c7c9e8a4728934ece08f977cd0
Milton Souza <milton.kowalewski@pgmais.com.br>
2023-10-08 17:13:57 -0300

    feat(master) update telescope config to ignore .git folder

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,7 +34,7 @@ local options = function()
         preview_cutoff = 120,
       },
       file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = { "node_modules", ".venv", ".git" },
+      file_ignore_patterns = { "node_modules", ".venv", ".git/" },
       generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
       path_display = { "truncate" },
       winblend = 0,
